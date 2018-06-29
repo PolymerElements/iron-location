e5cd32ae6465e7e3fcaa3cacfcd9a21c2d70f98e
Elliott Marquez <emarquez@google.com>
2018-06-29 13:54:44 -0700

    manual package cleanup

--- iron-query-params.js
+++ iron-query-params.js
@@ -6,13 +6,13 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-
-@demo demo/iron-query-params.html
 */
 import '@polymer/polymer/polymer-legacy.js';
 
 import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
-
+/**
+ * @demo demo/iron-query-params.html
+ */
 Polymer({
   is: 'iron-query-params',
 
